486f7912954d179b84e4003a87b056b6233a4be8
VolhaZha <olgazhauryd@gmail.com>
2023-07-30 18:54:49 +0200

    FinalTask_July30_1

--- src/test/java/generalTests/SuiteTest.java
+++
@@ -1,9 +0,0 @@
-package generalTests;
-
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({CreateAccTest.class, LogInTest.class, AddAdressTest.class, AddToWishListTest.class, AddToCartTest.class})
-public class SuiteTest {
-}
--- src/test/java/suiteTest/SuiteTest.java
+++ src/test/java/suiteTest/SuiteTest.java
@@ -1,12 +1,12 @@
 package suiteTest;
 
-import generalTests.LogInTest;
+import generalTests.*;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-//@SelectClasses({CreateAccTest.class, LogInTest.class, AddAdressTest.class, AddToWishListTest.class, AddToCartTest.class})
-@SelectPackages("generalTests")
+@SelectClasses({CreateAccTest.class, AddToCartTest.class, LogInTest.class, AddAdressTest.class,  AddToWishListTest.class})
+//@SelectPackages("generalTests")
 public class SuiteTest {
 }
